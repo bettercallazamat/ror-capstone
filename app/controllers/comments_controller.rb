3d2b780286dbfc8012282ae8e0bd6733ca0a3c05
bettercallazamat <bettercallazamat@gmail.com>
2020-10-17 04:29:44 +0600

    Fix opinions view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    @comment = current_user.comments.new(comment_params)
+    @comment = Comment.new(comment_params)
     @comment.opinion_id = params[:opinion_id]
+    @comment.user = current_user
     if @comment.save
       flash[:notice] = 'Comment was successfully created.'
-      redirect_to root_path
     else
-      flash[:alert] = 'Something went wrong...'
-      render root_path
+      flash[:alert] = 'Something went wrong.'
     end
+    redirect_to root_path
   end
 
   private
