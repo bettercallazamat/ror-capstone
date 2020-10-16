d3602b82dd2d26eab6f2a6a7ab0e0d096e6ad528
bettercallazamat <bettercallazamat@gmail.com>
2020-10-16 22:52:30 +0600

    Create controllers for followings

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,2 +1,12 @@
 class FollowingsController < ApplicationController
+  def create
+    @followed = User.find(params[:user_id])
+    @following = current_user.followings.new(followed_id: @followed.id)
+    if @following.save
+      flash[:notice] = "You started following #{@followed.fullname}"
+    else
+      flash[:alert] = 'Something went wrong ...'
+    end
+    redirect_to root_path
+  end
 end
