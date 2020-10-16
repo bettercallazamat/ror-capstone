82c3af151c63754d1184197d6b9be08e789b8aea
bettercallazamat <bettercallazamat@gmail.com>
2020-10-16 21:26:53 +0600

    Create routues for basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'opinions#index'
+
+  resources :users, only: %i[new create]
+
+  resources :opinions, only: %i[index create] do
+    resources :comments, only: [:create]
+  end
+
+  post 'opinion', to: 'opinions#create'
+  post 'follow', to: 'followings#create'
+
+  get 'users/:id', to: 'users#show', as: 'user'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'log_out', to: 'sessions#destroy'
 end
---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_16_145935) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "opinion_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["opinion_id"], name: "index_comments_on_opinion_id"
+  end
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "opinions", force: :cascade do |t|
+    t.text "text"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_opinions_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "fullname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "comments", "users"
+  add_foreign_key "opinions", "users", column: "author_id"
+end
