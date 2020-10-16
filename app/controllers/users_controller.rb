0bdaaae646b696c29d513eaf14140a98c4145658
bettercallazamat <bettercallazamat@gmail.com>
2020-10-16 21:54:20 +0600

    Add user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(signup_params)
+    @user = User.new(user_params)
     if @user.save
-      session[:user_iid] = @user.id
+      session[:user_id] = @user.id
       flash[:notice] = 'Account is created.'
       redirect_to root_path
     else
@@ -17,5 +17,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :fullname)
   end
 end
