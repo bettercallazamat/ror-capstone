9afd162fca1979880c74fff17232765629a14509
bettercallazamat <bettercallazamat@gmail.com>
2020-10-17 15:40:01 +0600

    Fix sign in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:session][:username])
 
     if @user
       session[:user_id] = @user.id
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:username] = nil
+    session[:user_id] = nil
     flash[:notice] = 'You have successfully logged out'
     redirect_to sign_in_path
   end
