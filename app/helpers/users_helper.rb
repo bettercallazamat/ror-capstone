efaa6ff7b8c41ce119cb69e2bd5353d3a73dd7e0
bettercallazamat <bettercallazamat@gmail.com>
2020-10-17 17:07:56 +0600

    Add link to last follower in who to follow

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def last_follower(user)
+    res = ''
+    if user.followers.any?
+      follower = user.followers.last
+      res = 'Followed by ' + link_to(follower.fullname, user_path(follower.id))
+    else
+      res = 'No followers'
+    end
+    res.html_safe
+  end
 end
