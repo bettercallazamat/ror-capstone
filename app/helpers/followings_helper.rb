37aafdeb32006579997e35805f854f17fc6c601d
bettercallazamat <bettercallazamat@gmail.com>
2020-10-20 22:05:33 +0600

    Fix rubocop issues

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -3,9 +3,9 @@ module FollowingsHelper
     return nil unless current_user.id != user.id
 
     if current_user.followeds.include?(user)
-      link_to "-", unfollow_path(user), method: :delete, class: "unfollow-btn"
+      link_to '-', unfollow_path(user), method: :delete, class: 'unfollow-btn'
     else
-      link_to "+", follow_path(user), method: :post, class: "follow-btn"
+      link_to '+', follow_path(user), method: :post, class: 'follow-btn'
     end
   end
 end
