81d2512edb86bc96d26d768d24285c7ecebc2536
bettercallazamat <bettercallazamat@gmail.com>
2020-10-20 22:42:29 +0600

    Add default user photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   has_one_attached :photo
+  has_one_attached :cover_image
 
   has_many :followings, class_name: 'Following', foreign_key: 'follower_id'
   has_many :followeds, through: :followings, source: :followed
@@ -16,4 +17,22 @@ class User < ApplicationRecord
   def who_to_follow
     User.where.not(id: id).where.not(id: followeds).order('created_at DESC')
   end
+
+  after_commit :add_default_photo, on: :create
+
+  private 
+
+  def add_default_photo 
+    unless photo.attached?
+      self.photo.attach(io: File.open(Rails.root.join("app", "assets", "imgs", "default-user.jpg")), filename: 'default.jpg' , content_type: "image/jpg")
+    end
+  end
+
+  def add_default_photo 
+    unless cover_image.attached?
+      self.photo.attach(io: File.open(Rails.root.join("app", "assets", "imgs", "default-cover.jpg")), filename: 'default.jpg' , content_type: "image/jpg")
+    end
+  end
+
+
 end
