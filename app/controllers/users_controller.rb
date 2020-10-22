43bec7c39213b4a74a0baba800e98074725c91c6
bettercallazamat <bettercallazamat@gmail.com>
2020-10-23 02:27:33 +0600

    Change saving user process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save!
+    if @user.save
       session[:user_id] = @user.id
       flash[:notice] = 'Account is created.'
       redirect_to root_path
